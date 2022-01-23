e139fbb0de93c363dc9657339fa5d75d2a4654a3
rkbling <raysonkaelbling@outlook.com>
2022-01-22 22:34:51 -0600

    Remove https

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const db = require("./database");
 const dingo = require("./dingo");
 const express = require("express");
 const fs = require("fs");
-const https = require("https");
 const rateLimit = require("express-rate-limit");
-const tls = require("tls");
 const { createDeflateRaw } = require("zlib");
 
 function asyncHandler(fn) {
@@ -178,16 +176,7 @@ const diff = async (height) => {
     })
   );
 
-  server = https.createServer({
-    key: fs.readFileSync('/etc/letsencrypt/live/bewp0.dingocoin.org/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/bewp0.dingocoin.org/fullchain.pem'),
-    SNICallback: (domain, cb) => {
-      cb(null, tls.createSecureContext({
-        key: fs.readFileSync('/etc/letsencrypt/live/bewp0.dingocoin.org/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/bewp0.dingocoin.org/fullchain.pem'),
-      }));
-    }
-  }, app).listen(8443, () => {
-    console.log(`Started on port 8443`);
+  app.listen(8080, () => {
+    console.log(`Started on port 8080`);
   });
 })();
